731d428e562dd5da93496a6a16e3b55ad5bf78ba
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-22 16:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let sum = 0;
+	while(arr.length > 0){
+		arr.sort((a,b) => a-b);
+		let num1 = arr[0];
+		if(arr.length >= 2){
+			num1+=arr[1];
+		}
+		sum+=num1;
+		arr.shift();
+		arr.shift();
+		if(arr.length <= 0){
+			break;
+		}
+		arr.push(num1);
+	}
+  return sum;
 }
 
 module.exports=mincost;
